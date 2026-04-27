372f9b65f1d8b36a4f227a6e8e05a87058fee93d
Adam Long <adam.jacob.long@gmail.com>
2026-04-27 17:33:19 -0500

    Added test renaming

--- controllers/clean_dataset.py
+++ controllers/clean_dataset.py
@@ -15,7 +15,7 @@ from controllers.data_receiver import *
 from controllers.csv_controller import *
 
 
-def fix_outlier_features():
+def fix_features():
     '''
     Removes features that have no meaning towards the goal of the model.
 
@@ -23,13 +23,13 @@ def fix_outlier_features():
     Renames features to be more descriptive.
     :return:
     '''
-    dataframe = get_csv_data(frametype='train')
-    dataframe.drop(
-        columns = ['Application_mode', 'Application_type'],
+    dataframe_train = get_csv_data(frametype='train')
+    dataframe_train.drop(
+        columns = ['id', 'Application_mode', 'Application_type'],
         inplace = True
     )
 
-    dataframe.rename(columns={
+    dataframe_train.rename(columns={
         'Course' : 'Course Code',
         'Nacionality' : 'Nationality',
         'Displaced' : 'Relocated',
@@ -41,4 +41,24 @@ def fix_outlier_features():
         'Curricular units 2nd sem (approved)' : 'Curricular units 2nd sem (passed)',
         'Curricular units 1st sem (without evaluations)' : 'Curricular units 1st sem (skipped exams)',
         'Curricular units 2nd sem (without evaluations)' : 'Curricular units 2nd sem (skipped exams)',
+    })
+
+    dataframe_test = get_csv_data(frametype='test')
+    dataframe_test.drop(
+        columns = ['id', 'Application_mode', 'Application_type'],
+        inplace = True
+    )
+
+    dataframe_test.rename(columns={
+        'Course': 'Course Code',
+        'Nacionality': 'Nationality',
+        'Displaced': 'Relocated',
+        'Curricular units 1st sem (credited)': 'Curricular units 1st sem (transferred)',
+        'Curricular units 2nd sem (credited)': 'Curricular units 2nd sem (transferred)',
+        'Curricular units 1st sem (evaluations)': 'Curricular units 1st sem (exams taken)',
+        'Curricular units 2nd sem (evaluations)': 'Curricular units 2nd sem (exams taken)',
+        'Curricular units 1st sem (approved)': 'Curricular units 1st sem (passed)',
+        'Curricular units 2nd sem (approved)': 'Curricular units 2nd sem (passed)',
+        'Curricular units 1st sem (without evaluations)': 'Curricular units 1st sem (skipped exams)',
+        'Curricular units 2nd sem (without evaluations)': 'Curricular units 2nd sem (skipped exams)',
     })
